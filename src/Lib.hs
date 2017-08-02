1c4ec444945a3bc5561276661889e3da9245c596
Teodor Heggelund <teodor.heggelund@gmail.com>
2017-08-03 00:26:40 +0200

    Sucessfully demote headers

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,4 +10,12 @@ someFunc = toJSONFilter $ texasRanger
 -- someFunc = putStrLn "someFunc"
 
 texasRanger :: Pandoc -> Pandoc
-texasRanger p = p
+texasRanger (Pandoc meta blocks) = Pandoc meta (map handleblock blocks)
+
+handleblock :: Block -> Block
+handleblock (Header 1 attrs inlines) = Header 2 attrs (capitalizeInlines inlines)
+handleblock nonlevel1header = nonlevel1header
+
+capitalizeInlines inlines = inlines
+
+-- Now, let's see if we can unwrap that Pandoc to find some headers.
---
+++ test/testfile.md
@@ -0,0 +1,12 @@
+# First header
+
+Some paragraph in between.
+
+This doesn't really matter.
+
+# Second header
+
+- Item
+- Another item
+
+# Third item
