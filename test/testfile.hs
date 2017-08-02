c5b00984511448ecd4ac80e3d31102690d6531a0
Teodor Heggelund <teodor.heggelund@gmail.com>
2017-08-03 01:28:04 +0200

    Parse spec file to internal document representation

---
+++ test/testfile.hs
@@ -0,0 +1,10 @@
+[Header 1 ("first-header",[],[]) [Str "First",Space,Str "header"]
+,Para [Str "Some",Space,Str "paragraph",Space,Str "in",Space,Str "between."]
+,Para [Str "This",Space,Str "doesn't",Space,Str "really",Space,Str "matter."]
+,Header 2 ("forslag-til-vurderingskriterier",["challenge"],[]) [Str "Forslag",Space,Str "til",Space,Str "vurderingskriterier"]
+,BulletList
+ [[Plain [Str "Item"]]
+ ,[Plain [Str "Another",Space,Str "item"]]]
+,CodeBlock ("",["python"],[]) "def f(x):\n    return x + 9"
+,Header 2 ("forutsetninger-og-utstyr",["challenge"],[]) [Str "Forutsetninger",Space,Str "og",Space,Str "utstyr"]
+,Header 1 ("third-item",[],[]) [Str "Third",Space,Str "item"]]
